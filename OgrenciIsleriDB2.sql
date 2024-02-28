1ceb49bfc2a85369eefaa7786e12a7a66bb538d4
KazimDandir <kikbal.dandir@gmail.com>
2024-02-28 13:23:07 +0300

    new files added

--- OgrenciIsleriDB2.sql
+++ OgrenciIsleriDB2.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE OgrenciIsleriDB
+CREATE DATABASE OgrenciIsleriDB2
 GO
-USE OgrenciIsleriDB
+USE OgrenciIsleriDB2
 GO
 CREATE TABLE tblOgrenciIsleri
 (
@@ -44,8 +44,7 @@ OgrNo INT FOREIGN KEY REFERENCES tblOgrenciBilgileri(OgrenciNo),
 DersNo INT FOREIGN KEY REFERENCES tblBolumDersleri(DersKodu),
 Vize INT,
 Final INT,
-Sonuc INT,
-Durumu VARCHAR(5)
+Durumu NVARCHAR(MAX)
 )
 GO
 CREATE TABLE tblOgrenci_Devam
@@ -53,7 +52,7 @@ CREATE TABLE tblOgrenci_Devam
 OgrNo INT FOREIGN KEY REFERENCES tblOgrenciBilgileri(OgrenciNo),
 Devamsizlik INT
 )
-GO
+
 
 --INSERTS
 INSERT INTO tblOgrenciIsleri(PersonelAd,PersonelSoyad)
@@ -62,17 +61,17 @@ VALUES ('Ali', 'Veli'),
 	   ('Enes', 'Yolda'), 
 	   ('Ebru', 'Kenarlý'),
 	   ('Buse', 'Türk')
-GO
+ 
 INSERT INTO tblBolumBilgileri (BolumAdi)
 VALUES ('Havacýlýk Yönetimi'),
        ('Gastronomi'),
 	   ('Psikoloji'),
 	   ('Bankacýlýk'),
 	   ('Mimarlýk')
-GO
+ 
 INSERT INTO tblBolumDersleri(DersAdý,BolumNo) 
 VALUES --Havacýlýk Yönetimi
-	   ('Ramp Hizmetleri', 1), ('Tehlikeli Maddeler', 1), ('Havacýlýk Ýngilizcesi', 1), ('Havacýlýk Ýþletme Prensipleri', 1), ('Havayolu Ýþletmeciliði', 1), ('Havaalaný Yönetimi',1), ('Hava Taþýmacýlýðý Ekonomisi',1), ('Havacýlýk Güvenliði ve Emniyeti',1),
+	   ('Ramp Hizmetleri', 1), ('Tehlikeli Maddeler', 1), ('Havacýlýk Ýngilizcesi', 1), ('Havacýlýk Ýþletme Prensipleri', 1), ('Havayolu Ýþletmeciliði', 1), ('Havaalaný Yönetimi',1), ('Hava Taþýmacýlýðý	  	 Ekonomisi',1), ('Havacýlýk Güvenliði ve Emniyeti',1),
 	   --Gastronomi
 	   ('Temel Mutfak Teknikleri', 2), ('Gýda Güvenliði ve Hijyen', 2), ('Gastronomik Tarih ve Kültür', 2), ('Beslenme Bilimi', 2), ('Yemek Sanatlarý ve Sunumu', 2), ('Mutfak Yönetimi ve Ýþletme', 2) , ('Gastronomik Ýnovasyon', 2), ('Gastronomik Geziler ve Deneyimler', 2),
 	   --Psikoloji
@@ -81,7 +80,7 @@ VALUES --Havacýlýk Yönetimi
 	   ('Bankacýlýða Giriþ', 4), ('Finansal Pazarlar ve Kurumlar', 4), ('Risk Yönetimi', 4), ('Banka Operasyonlarý', 4), ('Banka Yönetimi', 4), ('Kredi Analizi ve Deðerleme', 4), ('Finansal Düzenleme ve Denetim', 4),	('Bankacýlýk ve Teknoloji' ,4), ('Uluslararasý Bankacýlýk', 4),
 	   --Mimarlýk
 	   ('Temel Tasarým', 5), ('Mimari Tarih ve Teorisi', 5), ('Mimari Tasarým Stüdyosu', 5), ('Yapý Bilgisi ve Malzemeleri', 5), ('Mimarlýk Stüdyosu', 5), ('Mimarlýk Çizim ve Sunum Teknikleri', 5), ('Kentsel Tasarým ve Planlama', 5), ('Çevresel ve Sürdürülebilir Tasarým', 5), ('Mimarlýk ve Toplum', 5)   
-GO
+ 
 INSERT INTO tblPersonel_Ders(PersonelNo, DersKodu)
 VALUES --Personel1: Ali Veli
        (1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8),
@@ -93,7 +92,7 @@ VALUES --Personel1: Ali Veli
        (4, 27), (4, 28), (4, 29), (4, 30), (4, 31), (4, 32), (4, 33), (4, 34), (4, 35),
        --Personel5: Buse Türk
        (5, 36), (5, 37), (5, 38), (5, 39), (5, 40), (5, 41), (5, 42), (5, 43), (5, 44)
-GO
+ 
 INSERT INTO tblOgrenciBilgileri(OgrAd,OgrSoyad,DogumTarihi,Cinsiyet,EPosta)
 VALUES ('Ayþe', 'Kara', 15-03-1997, 'Kadýn', 'aysekara@mail.com'),
 	   ('Mehmet', 'Demir', 22-07-199, 'Erkek', 'mehmetdemir@mail.com'),
@@ -104,27 +103,36 @@ VALUES ('Ayþe', 'Kara', 15-03-1997, 'Kadýn', 'aysekara@mail.com'),
 	   ('Aylin', 'Kurt', 17-08-1995, 'Kadýn', 'aylinkurt@mail.com'),
 	   ('Ömer', 'Yýldýrým', 08-06-1996, 'Erkek', 'omeryildirim@mail.com'),
 	   ('Melis', 'Þen', 25-02-1998, 'Kadýn', 'melissen@mail.com')
-GO
-INSERT INTO tblOgrenci_Ders(OgrNo,DersNo,Vize,Final,Sonuc,Durumu)
-VALUES (1, 1, 50, 20, (50*0.4 + 20*0.6), 'Kaldý'),
-	   (1, 10, 70, 40, (70*0.4 + 40*0.6), 'Geçti'),
-	   (2, 4, 60, 46, (60*0.4 + 46*0.6), 'Geçti'),
-	   (2, 7, 35, 78, (35*0.4 + 78*0.6), 'Geçti'),
-	   (3, 15, 49, 79, (49*0.4 + 79*0.6) ,'Geçti'),
-	   (3, 6, 41, 81, (41*0.4 + 81*0.6) ,'Geçti'),
-	   (4, 22, 12, 78, (12*0.4 + 78*0.6) ,'Geçti'),
-	   (4, 9, 56, 92, (56*0.4 + 92*0.6) ,'Geçti'),
-	   (5, 41, 50, 60, (50*0.4 + 60*0.6) ,'Geçti'),
-	   (5, 4, 70, 40, (70*0.4 + 40*0.6) ,'Geçti'),
-	   (6, 19, 60, 46, (60*0.4 + 46*0.6) ,'Geçti'),
-	   (6, 27, 27, 78, (27*0.4 + 78*0.6) ,'Geçti'),
-	   (7, 21, 49, 79, (49*0.4 + 79*0.6) ,'Geçti'),
-	   (7, 35, 41, 81, (41*0.4 + 81*0.6) ,'Geçti'),
-	   (8, 11, 35, 42, (35*0.4 + 42*0.6) ,'Kaldý'),
-	   (8, 25, 56, 92, (56*0.4 + 92*0.6) ,'Geçti'),
-	   (9, 14, 50, 60, (50*0.4 + 60*0.6) ,'Geçti'),
-	   (9, 33, 49, 25, (49*0.4 + 25*0.6) ,'Kaldý')
-GO
+ 
+CREATE TRIGGER HesaplaOrtalama
+ON tblOgrenci_Ders
+AFTER INSERT, UPDATE
+AS
+BEGIN
+  UPDATE tblOgrenci_Ders
+  SET Durumu = CASE WHEN Vize IS NOT NULL AND Final IS NOT NULL THEN CASE WHEN ROUND(((Vize * 0.4) + (Final * 0.6)), 0) >= 50 THEN 'Geçti (' + CAST(ROUND(((Vize * 0.4) + (Final * 0.6)), 0) AS VARCHAR(5)) + ')' ELSE 'Kaldý (' + CAST(ROUND(((Vize * 0.4) + (Final * 0.6)), 0) AS VARCHAR(5)) + ')' END ELSE NULL END
+END
+
+INSERT INTO tblOgrenci_Ders(OgrNo,DersNo,Vize,Final,Durumu)
+VALUES (1, 1, 50, 20, NULL),
+	   (1, 10, 70, 40, NULL),
+	   (2, 4, 60, 46, NULL),
+	   (2, 7, 35, 78, NULL),
+	   (3, 15, 49, 79, NULL),
+	   (3, 6, 41, 81, NULL),
+	   (4, 22, 12, 78, NULL),
+	   (4, 9, 56, 92, NULL),
+	   (5, 41, 50, 60, NULL),
+	   (5, 4, 70, 40, NULL),
+	   (6, 19, 60, 46, NULL),
+	   (6, 27, 27, 78, NULL),
+	   (7, 21, 49, 79, NULL),
+	   (7, 35, 41, 81, NULL),
+	   (8, 11, 35, 42, NULL),
+	   (8, 25, 56, 92, NULL),
+	   (9, 14, 50, 60, NULL),
+	   (9, 33, 49, 25, NULL) 
+ 
 INSERT INTO tblOgrenci_Devam(OgrNo, Devamsizlik)
 VALUES (1, 8), (2, 17), (3, 3), (4, 12), (5, 19), (6, 16), (7, 10), (8, 15), (9, 4)
 
@@ -140,17 +148,17 @@ SELECT * FROM tblPersonel_Ders
 
 
 --SORGU ÖRNEKLERÝ
---1)Tüm dersler arasýnda kalanlar öðrencileri getir (Ad-soyad-sonuc-bölüm adý,ders adý)
+--1)Tüm dersler arasýnda kalanlar öðrencileri getir (Ad-soyad-durum-bölüm adý,ders adý)
 SELECT ob.OgrAd + ' ' + ob.OgrSoyad AS OgrenciAdiSoyadi,
-	   od.Sonuc AS Ortalama,
 	   bb.BolumAdi,
-	   bd.DersAdý
+	   bd.DersAdý,
+	   od.Durumu
 FROM tblOgrenciBilgileri AS ob
 	JOIN tblOgrenci_Ders AS od ON ob.OgrenciNo=od.OgrNo
 	JOIN tblBolumDersleri AS bd ON od.DersNo=bd.DersKodu
 	JOIN tblBolumBilgileri AS bb ON bd.BolumNo=bb.BolumNo
-WHERE od.Durumu='Kaldý'
-ORDER BY od.Sonuc 
+WHERE od.Durumu LIKE '%Kaldý%'
+ORDER BY od.Durumu 
 
 --2)tblBolumDersleri tablosundaki DersAdý kolon adýný DersAdi olarak deðiþtir
 EXEC sp_rename 'dbo.tblBolumDersleri.DersAdý', 'DersAdi', 'COLUMN'; 
@@ -168,8 +176,7 @@ ORDER BY COUNT(bd.DersAdi) DESC
 UPDATE tblOgrenciIsleri SET PersonelSoyad='Yoldaþ' WHERE PersonelNo=3
 
 --5)Havacýlýk Yönetimi bölümündeki öðrencileri listele
-SELECT bb.BolumNo, 
-	ob.OgrAd + ' ' + ob.OgrSoyad AS [Öðrenci Adý ve Soyadý],
+SELECT ob.OgrAd + ' ' + ob.OgrSoyad AS [Öðrenci Adý ve Soyadý],
 	bb.BolumAdi AS [Bölüm Adý],
 	bd.DersKodu AS [Ders Kodu],
 	bd.DersAdi AS [Ders Adý]
